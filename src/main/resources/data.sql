90f9984d0fd022b8bcbafe3ae8f48bcd2eed5a10
fsclass-n <whyeil@naver.com>
2026-04-29 15:20:21 +0900

    프론트 완료

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO movies (title, description, image_url)
-VALUES
-  ('흥부', '조선 시대 전통 판타지', 'https://via.placeholder.com/200'),
-  ('늑대소녀', '늑대와 소녀의 사랑 이야기', 'https://via.placeholder.com/200');
