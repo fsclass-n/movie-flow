2aeecd2a7457c15e546bc8ba53e05a7ba118508c
fs-class <whyeil29@gmail.com>
2026-05-04 01:24:59 +0900

    rpa 수정

--- rpa/scripts/account.py
+++ rpa/scripts/account.py
@@ -1,5 +1,6 @@
 # account.py
 import os
+import smtplib
 from dotenv import load_dotenv
 
 # 현재 파일 위치 기준 .env 로드
@@ -9,4 +10,6 @@ load_dotenv()
 EMAIL_ADDRESS = os.getenv("MAIL_USER")
 EMAIL_PASSWORD = os.getenv("MAIL_PASS")
 SMTP_HOST = os.getenv("MAIL_HOST", "smtp.gmail.com")
-SMTP_PORT = int(os.getenv("MAIL_PORT", 587))
+# SMTP_PORT = int(os.getenv("MAIL_PORT", 587))
+# 변경 (SMTP_SSL)
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
