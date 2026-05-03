2a277e66b0472023d1a971ab01c831ca8836a6ba
fs-class <whyeil29@gmail.com>
2026-05-04 01:26:44 +0900

    포트수정

--- rpa/scripts/account.py
+++ rpa/scripts/account.py
@@ -1,15 +1,13 @@
-# account.py
 import os
-import smtplib
 from dotenv import load_dotenv
 
 # 현재 파일 위치 기준 .env 로드
 load_dotenv()
 
-# .env의 키값과 동일하게 수정
+# .env의 키값과 동일하게 수정 (Render Environment Variables 설정 필수)
 EMAIL_ADDRESS = os.getenv("MAIL_USER")
-EMAIL_PASSWORD = os.getenv("MAIL_PASS")
+EMAIL_PASSWORD = os.getenv("MAIL_PASS")  # 구글 앱 비밀번호 16자리
+
+# 클라우드 환경(Render) 안정성을 위해 SSL/465 고정 사용 권장
 SMTP_HOST = os.getenv("MAIL_HOST", "smtp.gmail.com")
-# SMTP_PORT = int(os.getenv("MAIL_PORT", 587))
-# 변경 (SMTP_SSL)
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+SMTP_PORT = int(os.getenv("MAIL_PORT", 465))
